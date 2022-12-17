41020f1120237d60d219aab4e39844102a82b3a1
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-12-17 07:21:21 +0530

    racket

---
+++ expression.rkt
@@ -0,0 +1,22 @@
+#!/usr/bin/racket
+#lang racket
+
+;simple arthimatic operation
+
+
+(+ 7 8) ;expression --->gives value 
+;rackets evaluates expression to produce values
+
+(+ 3 (* 3 3)) 
+(/ 12 (* 2 3))
+
+;all of them primitive operation on operands
+(sqr 4)
+(sqrt 16)
+
+
+;question write an expression for underroot 3**2 + 4 **2
+
+;answer
+
+(sqrt (+ (* 3 3) (* 4 4))) ;expression for solving pythogreous equation
